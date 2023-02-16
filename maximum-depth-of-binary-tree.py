1831bd80451df4c4527c054612a093810a7f50f9
Caleb Stromberg <calebstromberg@gmail.com>
2023-02-16 11:31:47 -0800

    Complete maximum-depth-of-binary-tree

---
+++ maximum-depth-of-binary-tree.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self) -> str:
+        return str(self.val)
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
