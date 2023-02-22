621ddd83083dabb3fe0f78d4cf43b0cab28dc126
Caleb Stromberg <calebstromberg@gmail.com>
2023-02-22 11:59:48 -0800

    Complete capacity-to-ship-packages-within-d-days

---
+++ capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def capacityWorks(cap):
+            curr = 0
+            count = 1
+            for weight in weights:
+                if weight + curr > cap:
+                    count += 1
+                    curr = weight
+                else:
+                    curr += weight
+                if count > days:
+                    return False
+            return True
+        
+        left = max(weights)
+        right = sum(weights)
+        while left < right - 1:
+            mp = (left + right) // 2
+            if capacityWorks(mp):
+                right = mp
+            else:
+                left = mp
+        return left if capacityWorks(left) else right
+
+s = Solution()
+print(s.shipWithinDays(weights = [3,2,2,4,1,4], days = 3))
