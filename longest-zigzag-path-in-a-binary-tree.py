f1334d56cd5d55c63ec5ec8d4f1f56cbad2fcbe3
Caleb Stromberg <calebstromberg@gmail.com>
2023-04-19 12:18:00 -0700

    Complete longest-zigzag-path-in-a-binary-tree

---
+++ longest-zigzag-path-in-a-binary-tree.py
@@ -0,0 +1,26 @@
+from util import TreeNode
+from typing import Optional
+
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        def getLeftZz(node):
+            if not hasattr(node, "leftzz"):
+                node.leftzz = 1 + getRightZz(node.left) if node.left else 0
+            return node.leftzz
+        
+        def getRightZz(node):
+            if not hasattr(node, "rightzz"):
+                node.rightzz = 1 + getLeftZz(node.right) if node.right else 0
+            return node.leftzz
+
+        self.best = 0
+
+        def getBestZz(node):
+            if not node:
+                return
+            self.best = max(self.best, getLeftZz(node), getRightZz(node))
+            getBestZz(node.left)
+            getBestZz(node.right)
+        
+        getBestZz(root)
+        return self.best
